e82bd97f56ad47071755b940cefa42cff88d7f4f
damianparchi <damian.parchimowicz99@gmail.com>
2022-12-10 12:11:04 +0100

    routing menu + widok kontakt i onas

--- appwww/app/urls.py
+++ appwww/app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home),
+    path('onas/', views.onas, name="onas"),
+    path('kontakt/', views.kontakt, name="kontakt"),
     path('kategorie/<slug:val>', views.KategorieView.as_view(), name="kategorie"),
-]
+    path('tytul-kategorii/<val>', views.KategorieView.as_view(), name="tytul-kategorii"),
+    path('product-desc/<int:pk>', views.ProductDesc.as_view(), name="product-desc"),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- appwww/app/views.py
+++ appwww/app/views.py
@@ -7,8 +7,25 @@ from django.db.models import Count
 def home(request):
     return render(request, "app/home.html")
 
+def onas(request):
+    return render(request, "app/onas.html")
+
+def kontakt(request):
+    return render(request, "app/kontakt.html")
+
 class KategorieView(View):
     def get(self, request, val):
         product = Product.objects.filter(kategoria = val)
         nazwa = Product.objects.filter(kategoria = val).values('nazwa').annotate(total=Count('nazwa'))
         return render(request, "app/category.html", locals())
+
+class nazwaKategorii(View):
+    def get(self, request, val):
+        product = Product.objects.get(nazwa = val)
+        nazwa = Product.objects.filter(kategoria = product[0].kategoria).values('nazwa')
+        return render(request, "app/kategoria.html", locals())
+
+class ProductDesc(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk = pk)
+        return render(request, "app/productdesc.html", locals())
